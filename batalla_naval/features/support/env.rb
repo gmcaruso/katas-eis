2b25457ae4556921ac76eee41439d75d0f49441c
Gabriel Caruso <gabrielmcaruso@gmail.com>
2015-05-28 13:19:26 -0300

    otro proyecto limpio

--- batalla_naval/features/support/env.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + "/../../config/boot")
-require 'capybara/cucumber'
-require 'rspec/expectations'
-require_relative '../../app/app.rb'
-
-# Capybara.default_driver = :selenium
-Capybara.app = Battleship::App.tap { |app|  }
