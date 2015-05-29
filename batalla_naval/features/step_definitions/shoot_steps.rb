3a1a59420fac1edb9e9f827bef6cd68d7b8ea9ff
Gabriel Caruso <gabrielmcaruso@gmail.com>
2015-05-28 23:16:36 -0300

    Agregar barco grande test, verde

--- batalla_naval/features/step_definitions/shoot_steps.rb
+++ batalla_naval/features/step_definitions/shoot_steps.rb
@@ -2,7 +2,16 @@ require_relative '../../app/models/tablero.rb'
 require_relative '../../app/models/barco.rb'
 
 Given(/^a large ship in position: "(.*?)" "(.*?)"$/) do |posicion1, posicion2|
-  	pending
+  	page.should have_content("Crear Barco Grande")
+	find_by_id("en_posiciones_uno_grande")
+	find_by_id("en_posiciones_dos_grande")
+
+	fill_in('en_posiciones_uno_grande', :with => posicion1)
+	fill_in('en_posiciones_dos_grande', :with => posicion2)
+
+	has_button?('Añadir Barco grande a flota').should eq true
+  	click_button('Añadir Barco grande a flota')
+  	page.should have_content("Barco agregado!")
 end
 
 Given(/^I shoot to position "(.*?)"$/) do |posicion|
