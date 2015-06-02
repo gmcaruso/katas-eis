3d604fe38cdc1ab56f83e6365f91e280ed703104
Gabriel Caruso <gabrielmcaruso@gmail.com>
2015-06-02 09:33:54 -0300

    Crear un barco chiquito -verde(chequea las dimensiones del barco)-

--- batalla_naval/app/models/tablero.rb
+++ batalla_naval/app/models/tablero.rb
@@ -10,9 +10,13 @@ class Tablero
   end
 
   def agregar_barco(barco)
-    posicionar_barco_en_radar(barco)
-    flota_de_barcos.push(barco)
-    "Barco agregado!"
+    if chequear_dimension_de(barco)
+      posicionar_barco_en_radar(barco)
+      flota_de_barcos.push(barco)
+      "Barco agregado!"
+    else 
+      "Barco fuera de mapa"
+    end
   end
 
   def posicionar_barco_en_radar(barco)
@@ -21,6 +25,17 @@ class Tablero
     end
   end
 
+  def chequear_dimension_de(barco)
+    entra_en_mapa= true
+    barco.posiciones.each do |posicion|
+      entra_en_mapa= esta_dentro_del_mapa(alto, ancho, posicion, entra_en_mapa)
+    end
+    entra_en_mapa
+  end
+
+  def esta_dentro_del_mapa(alto, ancho, posicion_del_barco, entra_en_mapa)
+    entra_en_mapa= entra_en_mapa && (posicion_del_barco[0].to_i < ancho.to_i && posicion_del_barco[2].to_i < alto.to_i)
+  end
 
   def alto
     @alto
